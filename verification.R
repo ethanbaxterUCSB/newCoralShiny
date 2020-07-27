a39e55e5c530935eca8365d08a92898e332dbe10
ethanbaxterUCSB <63381534+ethanbaxterUCSB@users.noreply.github.com>
2020-07-27 13:23:22 -0700

    Add files via upload

--- verification.R
+++ verification.R
@@ -1,6 +1,6 @@
 
 #------------------------------------------------------
-#Verification of runCoral and runCoral with master runs
+#Verification of runCoral and runCoralSS with master runs
 #------------------------------------------------------
 
 #My scripts
@@ -44,7 +44,7 @@ ss <- {
   
   pars_HX <- defPars_HX
   pars_S <- defPars_S(1)
-  env <- c(L = 30, N = 1e-7, X = 1e-7)
+  env <- c(L = 20, N = 1e-8, X = 1e-7)
   
   mySS <- runCoralSS(env, pars_HX, pars_S)
   
@@ -54,8 +54,45 @@ ss <- {
   source("master/def_pars.R")
   
   pars <- def_pars(1)
-  env <- list(L = 30, N = 1e-7, X = 1e-7)
+  env <- as.list(env)
   masterSS <- run_coral_ss(env, pars, dt = 0.1)
   
-  unlist(mySS$dH.Hdt)[length(unlist(mySS$dH.Hdt))] - masterSS$H$dH.Hdt[length(masterSS$H$dH.Hdt)]
+  print(unlist(mySS$dH.Hdt)[length(unlist(mySS$dH.Hdt))] - masterSS$H$dH.Hdt[length(masterSS$H$dH.Hdt)])
+}
+
+runOverVariedEnv <- {
+  source("runCoral.R")
+  source("defPars.R")
+  source("master/run_coral.R")
+  source("master/def_pars.R")
+  #Normal runs with constant env
+  x <- list(range = c(20,30), res = 1, name = "L")
+  y <- list(range = c(0, 1e-6), res = 1e-7, name = "X")
+  sens <- 0.001
+  
+  time <- seq(from = 0, to = 365, by = 0.1)
+  env <- c(L = 30, N = 1e-7, X = 1e-7)
+  myPars <- list(HX = defPars_HX, S = defPars_S(1))
+  masterPars <- def_pars(1)
+  
+  xAxis <- seq(from = x$range[1], to = x$range[2], by = x$res)
+  yAxis <- seq(from = y$range[1], to = y$range[2], by = y$res)
+  imageData <- matrix(nrow = length(xAxis), ncol = length(yAxis))
+  for (i in 1:length(xAxis)) {
+    print(paste("X", i))
+    env[x$name] <- xAxis[i]
+    for (j in 1:length(yAxis)) {
+      print(paste("Y", j))
+      env[y$name] <- yAxis[j]
+      newEnv <- environment(time, c(env["L"], env["L"], 0), c(env["N"], env["N"], 0), c(env["X"], env["X"], 0))
+      myCoral <- runCoral(time, newEnv, myPars$HX, myPars$S)
+      masterCoral <- run_coral(time, newEnv, masterPars)
+      imageData[i,j] <- abs(mean(myCoral$SH - masterCoral$S/masterCoral$H))
+    }
+  }
+  #Data map
+  image(xAxis, yAxis, imageData, zlim = c(0, sens), xlab = "L from 20 to 30 by 1", ylab = "X from 0 to 1e-6 by 1e-7", main = "Abs. difference")
+  #Scale
+  image(as.matrix(seq(from = 0, to = sens, by = sens/10)))
+  title(xlab = "L from 20 to 30 by 1", ylab = "X from 0 to 1e-6 by 1e-7", main = "Absolute of value of difference between runCoral.R and run_Coral.R on scale of 0 to 0.001")
 }
